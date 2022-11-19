5f0e6984c53ddf1efaa1f1f69b4316bbe392e874
tristansizik <tristan.sizik@gmail.com>
2022-11-18 19:17:10 -0800

    updated port to 0.0.0.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 var server = http.createServer(handleRequest);
